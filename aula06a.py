e0498f6669ef0d9cef720eb77598f64038a1841d
Leandro Meneses Azevedo <menezesdatoulon.lm@gmail.com>
2023-08-22 21:58:18 -0300

    Primeiros passos em python

---
+++ aula06a.py
@@ -0,0 +1,7 @@
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite um valor: '))
+s = n1 + n2
+print('A soma dos valores são: {}'.format(s))
+print('A soma entre {} e {} é {}'.format(n1, n2, s))
+print(type(s))# mostra o tipo da variavel string, int, float e etc...
+
---
+++ ex002.py
@@ -0,0 +1,3 @@
+nome = input("Digite seu nome: ")
+print('É um prazer te conhecer: ', nome)
+print('prazer te conhecer, {}!'.format(nome)) # essa também é outra forma de colocar uma variavel em string
