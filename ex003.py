b7e7b3af0272de51f40445dfb632b6e900e2940e
Leandro Meneses Azevedo <110135554+LeandroMenesesAzevedo@users.noreply.github.com>
2023-08-28 22:12:15 -0300

    resolvendo exemplo

---
+++ ex003.py
@@ -0,0 +1,9 @@
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+
+#Colocando o print com o cálculo dentro do .format para não criar muitas váriaveis.
+
+print('A soma entre os números digitados são: {}'.format(n1+n2))
+print('A subtração do números digitados são: {}'.format(n1-n2))
+print('A multiplicação dos números digitados são: {}'.format(n1*n2))
+print('A divisão dos números digitados são: {}'.format(n1/n2))
