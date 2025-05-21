86e82f7084b53c40eeeebd3659ed8b7d8160e1e1
Leandro Meneses Azevedo <menezesdatoulon.lm@gmail.com>
2025-05-20 21:15:33 -0300

    desafio do antecessor e sucessor

---
+++ desafio06.py
@@ -0,0 +1,8 @@
+print(' ============= Desafio antecessor e sucessor')
+n = int(input('Digite um número'))
+su = n + 1
+an = n - 1
+
+print('O número digitado foi: {}'.format(n))
+print('O seu sucessor é: {}'.format(su))
+print('O seu antecesso é: {}'.format(an))
