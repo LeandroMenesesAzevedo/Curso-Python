497ddf8e7a0ee8d9f7e1714f30c6f7ba91a6a785
Leandro Meneses Azevedo <menezesdatoulon.lm@gmail.com>
2025-05-22 20:08:37 -0300

    refazendo alguns exercícios

---
+++ teste/media.py
@@ -0,0 +1,8 @@
+print('Desafio da media')
+n1 = float (input('Digite a primeira nota: '))
+n2 = float (input('Digite a segunda nota: '))
+med = (n1+n2)/2
+
+print ('Nota 1 = {}'.format(n1))
+print ('Nota 2 = {}'.format(n2))
+print ('Media = {}'.format(med))
---
+++ teste/medida.py
@@ -0,0 +1,7 @@
+print ('============== Conversor Centímetros em Milímetros==================')
+md = int(input('Digite a medida em Centímetros: '))
+xc = md * 100
+xm = md * 1000
+
+print('O valor {} m é : valor {} cm'.format(md, xc))
+print('O valor {} m e : valor {} mm'.format(md, xm))
