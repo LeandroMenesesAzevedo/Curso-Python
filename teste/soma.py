79bfe3dfa97f23ac72d2d86f21ba13cc532e2fc4
Leandro Meneses Azevedo <menezesdatoulon.lm@gmail.com>
2025-05-27 19:47:03 -0300

    Organizando a pasta

--- teste/soma.py
+++ teste/soma.py
@@ -1,5 +1,6 @@
-print('========================== Soma entre dois números =========================')
-n1 = int(input('Digite um número: '))
-n2 = int(input('Digite outro número: '))
+n1 = int( input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+
 soma = n1 + n2
-print('A soma entre {} e {} = {}'.format(n1, n2, soma))
+
+print('A soma entre {} e {} é {}'.format( n1, n2, soma))
