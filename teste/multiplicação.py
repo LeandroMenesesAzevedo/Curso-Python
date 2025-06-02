1cda9f5b6282183d5c16acffc11df01e50f76650
Leandro Meneses Azevedo <110135554+LeandroMenesesAzevedo@users.noreply.github.com>
2025-06-02 19:19:12 -0300

    multiplicação
    O Dobro, Triplo e Raiz Quadrada

--- teste/multiplicação.py
+++ teste/multiplicação.py
@@ -3,7 +3,7 @@ n = int (input('Digite um valor: '))
 
 d = n * 2
 t = n * 3
-r = n * n
+r = n ** (1/2)
 
 print ('O valor digitado é: {}, o dobro é: {}, o triplo é: {} e raiz {}'.format(n, d , t, r))
 
