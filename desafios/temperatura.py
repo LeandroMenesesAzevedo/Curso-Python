b5fa8cabc9f737bb95663c878da90efb1aeacd23
Leandro Meneses Azevedo <menezesdatoulon.lm@gmail.com>
2025-07-02 18:29:55 -0300

    Conversor de temperatura

---
+++ desafios/temperatura.py
@@ -0,0 +1,4 @@
+print('******************** Conversor de temperatura *********************')
+ce = float (input ( ' Digite a temperatura em °C: '))
+fa = (( 9 * ce ) / 5 ) + 32
+print ( 'A temperatura digita foi {}°C, temperatura convertida para {}°F'.format( ce, fa) )
