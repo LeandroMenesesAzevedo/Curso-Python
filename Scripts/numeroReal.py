af85bab8b43a2e165ac239394c485b2dc55b582b
Leandro Meneses Azevedo <menezesdatoulon.lm@gmail.com>
2025-07-09 19:24:03 -0300

    inserindo comentário

--- Scripts/numeroReal.py
+++ Scripts/numeroReal.py
@@ -1,4 +1,4 @@
-from math import trunc
+from math import trunc # importamos dentro da biblioteca  math a função trunc que retorna a parte inteira do número
 
 num = float( input (' Digite um número: '))
 print ('O valor digitado foi {} e o valor inteiro é: {}'.format(num, trunc(num)))
