c9c5ac2b6619b58567381a3324a0a049c56a84e9
Leandro Meneses Azevedo <menezesdatoulon.lm@gmail.com>
2025-07-15 21:47:59 -0300

    Correção do exercício

--- desafios/angulo.py
+++ desafios/angulo.py
@@ -1,9 +1,13 @@
-import math
+from math import radians , sin, cos,tan
+# Correção do exercício adicionei a função radians para os valores radianos.
+# inserido a função sin = cálculo seno
+# inserido a função cos = cáculo cosseno
+# inserido a função tan = cálculo tangente
 
 an = float ( input ( 'Digite o angulo: ' ) )
-se = math.sin(an)
-co = math.cos(an)
-ta = math.tan(an)
+se = sin(radians(an))
+co = cos(radians(an))
+ta = tan(radians(an))
 
 print ( 'O angulo digitado foi {:.0f}'.format(an) )
 print ( 'O seno é: {:.2f} '.format(se) )
