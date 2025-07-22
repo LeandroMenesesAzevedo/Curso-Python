ccbf5f0c25ec4d657e92651ff165ccb838cc4911
Leandro Meneses Azevedo <menezesdatoulon.lm@gmail.com>
2025-07-22 19:41:55 -0300

    Fazendo Manipulação de frase

--- desafios/manifrase.py
+++ desafios/manifrase.py
@@ -3,3 +3,6 @@ print(frase)
 print(frase[3]) #Vai imprimir o s a quarta letra do indice
 print(frase[0:6]) #Irá imprimir a palavra curso pois o c é indice 0 e o é indice 5
 print(frase[0:12:2])#Irá imprimir até a letra e pulando de dois em dois
+print(frase.count(o))#Irá contar quantas letras o terá
+print(len(frase))
+
