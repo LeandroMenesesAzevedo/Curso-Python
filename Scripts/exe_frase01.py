7b9bdaac9cc8d846839878f95080dbae7170dd62
Leandro Meneses Azevedo <menezesdatoulon.lm@gmail.com>
2025-07-24 20:51:16 -0300

    Desafio manipulando frase

---
+++ Scripts/exe_frase01.py
@@ -0,0 +1,11 @@
+frase = input('Digite o seu nome completo: ')
+
+print (frase)
+print ('Todas as letras do seu nome com letra maiúscula:')
+print (frase.upper())
+print ('Todas as letra do seu nome com letra minúscula:')
+print (frase.lower())
+print ('Quantidade de letra do primeiro nome :')
+print ( len ( frase.split()[0]) )
+
+print ( len ( frase.replace (' ')) )
