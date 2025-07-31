eb2a1154bcf9e25845ad9274bd90eabf698f57e3
Leandro Meneses Azevedo <menezesdatoulon.lm@gmail.com>
2025-07-30 21:51:14 -0300

    Exercício 22
    Manipulando frase

--- Scripts/exe_frase01.py
+++ Scripts/exe_frase01.py
@@ -9,6 +9,10 @@ print ('Quantidade de letra do primeiro nome :')
 print ('O Tamanho do texto sem espaço é: {}'.format(len(frase) - frase.count(' ')))
 # Como podemos observar ultilizei a função len - count para retirar contar o tamanho do nome sem espaço no meio
 # função count conta os espaço
-print('Seu primeiro nome tem {} letras'.format(frase.find(' ')))
+#print('Seu primeiro nome tem {} letras'.format(frase.find(' ')))
+separa = frase.split()
+#ultilizamos a função split para separa as palavras da frase e colocar em uma lista
+#chamamo o separa na posição 0 que mostra o primeiro nome.
+print('Seu primeiro nome é {} e ele possui {} letras'.format(separa[0], len(separa[0])))
 
 
