d0cc187d5ce0e31f742216a4d66a50fd2b014c48
Leandro Meneses Azevedo <menezesdatoulon.lm@gmail.com>
2025-07-31 20:34:13 -0300

    Exercício 25

---
+++ Scripts/exe25.py
@@ -0,0 +1,5 @@
+nome = str (input('Digite seu nome completo: ')).strip()
+#Ultilizamos a função strip para eliminarmos os espaços antes e depois da frase
+
+print ('Seu Nome exite Silva? {}'.format('SILVA' in nome.upper()))
+#Ultilizamos um operador do phyton para ver consta o nome Silva dentro da frase 
