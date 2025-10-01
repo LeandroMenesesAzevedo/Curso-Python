309e55855e7005650b6e19dd73cd1a1cfdcad80e
Leandro Meneses Azevedo <110135554+LeandroMenesesAzevedo@users.noreply.github.com>
2025-09-30 21:14:15 -0300

    Atualizando o jogo de adivinhação

--- Scripts/exe028.py
+++ Scripts/exe028.py
@@ -1,7 +1,14 @@
-num = int(input('Digite um número: '))
-
-if num == 0 | num <= 5:
+from random import randint
+computador = randint(0 , 5) # FAZ O COMPUTADOR SORTEAR UM NÚMERO DE 0 A 5
+print('*' * 20)
+num = int(input('Tente adivinhar um número que estou pensando: '))
+print('*' * 20)
+print ('PROCESSANDO ...')
+if num == computador:
+    print('PARABÉNS VOCÊ ACERTOU! ')
     print(' O número que estou pensando é {}'.format(num))
 else:
-    print ('O número que estou pensando é 5.')
+    print('VOCÊ PERDEU')
+    print ('O número que estou pensando é {} e não {}.'.format(computador, num))
+
 
