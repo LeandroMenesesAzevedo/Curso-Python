b150eac914cee721150ad57feb981e5c60a43ed0
Leandro Meneses Azevedo <110135554+LeandroMenesesAzevedo@users.noreply.github.com>
2025-10-06 20:47:36 -0300

    exe29

--- Scripts/exe029.py
+++ Scripts/exe029.py
@@ -1,8 +1,9 @@
 print (' ========== Sistema de transito ===========')
 
-vel = int(input('Qual a velocidade atual? '))
+vel = float(input('Qual a velocidade atual? '))
 if vel > 80:
     multa = (vel - 80) * 7
+    print('MULTADO você passou acima do limite de velocidade da via que é 80km/h')
     print('Você foi multado em R$ {:.2f}'.format(multa))
-else:
-    print ('Parabéns, conduza com segurança.')
+
+print ('Tenha um  bom dia e dirija com segurança')
