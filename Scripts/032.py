e56fb861dc9ccc9656ad3435046517a836728ac2
Leandro Meneses Azevedo <110135554+LeandroMenesesAzevedo@users.noreply.github.com>
2025-10-06 21:12:24 -0300

    Atualizando os exercícios

--- Scripts/032.py
+++ Scripts/032.py
@@ -1,12 +1,14 @@
+from datetime import date #importamos uma biblioteca para pegar o ano atual.
+
 print ( '==========  Origem dos anos ==========')
-
 ano = int( input ( ' Digite o ano que deseja: '))
-
-if ano % 4 == 0 and ano % 100 != 0:
+if ano == 0:
+    ano = date.today().year
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
     print (' O ano é bissexto')
 else:
     if ano % 400 == 0:
-        print (' O ano é bissexto')
+        print (' O ano {} é bissexto'.format(ano))
     else:
-        print (' O ano não é bissexto')
+        print (' O ano {} não é bissexto'.format(ano))
 
--- Scripts/exe031.py
+++ Scripts/exe031.py
@@ -6,7 +6,7 @@ if distancia <= 200:
     print (' O valor da viagem é R$ {:.2f}'.format(valor))
 else:
     valor  = distancia * 0.45
-    print (' O valor percorrido foi{} KM'.format(distancia))
+    print (' O valor percorrido foi {} KM'.format(distancia))
     print (' O valor da viagem é R$ {:.2f}'.format(valor))
 
 print('==========  Tenha uma boa Viagem ===========')
