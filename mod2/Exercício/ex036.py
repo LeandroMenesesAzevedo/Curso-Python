acac40d20c261b216ddc651ff386795670f224f9
Leandro Meneses Azevedo <menezesdatoulon.lm@gmail.com>
2026-01-05 20:34:57 -0300

    Modificando o exercício 36

--- mod2/Exercício/ex036.py
+++ mod2/Exercício/ex036.py
@@ -1,13 +1,13 @@
 casa = float (input ('Qual o valor do imóvel: '))
 sal = float (input ('Qual o seu salário: '))
-meses = int (input('Quantos meses deseja pagar: '))
-parcela = casa * ( 0.5 * (1 + 0.5)** meses) / (( 1 + 0.5)** meses -1)
+anos = int (input('Quantos meses deseja pagar: '))
+parcela = casa / (anos * 12)
+minimo = sal *30 / 100
+if parcela <= minimo:
 
-if sal < pm:
+    print (' O valor da parcela será R$ {:.2f} em {} anos'.format(parcela, anos))
+    print (' Emprestimo CONCEDIDO')
 
-    print (' O valor da parcela será R$ {:.2f} em {} anos'.format(parcela, meses))
-    print (' O financiamento ocorre um juros de 5%')
-
-elif sal > pm:
+else:
     print('Seu emprestímo foi negado.')
     print(' Infelizmente você não poderá financiar está casa')
