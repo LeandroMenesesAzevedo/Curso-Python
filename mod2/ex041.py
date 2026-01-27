4ade369778295185c2e7eeff1a1f66d90d327b3c
Leandro Meneses Azevedo <110135554+LeandroMenesesAzevedo@users.noreply.github.com>
2026-01-27 20:31:38 -0300

    Finalizado Exercício 041

--- mod2/ex041.py
+++ mod2/ex041.py
@@ -1,10 +1,17 @@
-print
+from datetime import date # utilizado para pegar o ano atual.
+print ('Confederação Nacional de Natação')
 
-ano = int(input('Digite o ano de nascimento: '))
+nasc = int(input('Digite o ano de nascimento do atleta: '))
+atual = date.today().year # pegando o ano atual
+idade = atual - nasc
 
-if ano <= 17:
-    print ('Sua Classificação é Junior.')
-elif ano >= 18:
-    print (' Sua Clssificação é Prifissional')
-elif ano > 38:
-    print (' Está Aposentado')
+if idade <= 9:
+    print ('Sua Classificação é Mirim.')
+elif idade > 9 and idade <= 14:
+    print (' Sua Classificação é Infatil')
+elif idade > 14 and idade <= 19:
+    print (' Sua Classificação é Junior')
+elif idade > 19 and idade <= 25:
+    print (' Sua Classificação  é Sênior')
+elif idade > 25:
+    print('Sua Classificação é Master')
