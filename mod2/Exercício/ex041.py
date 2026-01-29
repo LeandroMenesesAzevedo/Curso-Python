a3855cce3be4ff579fadecbedf303f7319c1750c
Leandro Meneses Azevedo <menezesdatoulon.lm@gmail.com>
2026-01-29 20:59:16 -0300

    Condição de existencia de um triângulo

---
+++ mod2/Exercício/ex041.py
@@ -0,0 +1,7 @@
+print( 'Condição de existencia de um triângulo')
+
+la = int(input( 'Digite o valor do primeiro lado: '))
+lb = int(input( 'Digite o valor do segundo lado: '))
+lc = int(input( 'Digite o valor do terceiro lado: '))
+
+if ()
