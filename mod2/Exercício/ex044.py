5ec4c58af61408f0097f0a1f7b14be73f4dfecec
Leandro Meneses Azevedo <menezesdatoulon.lm@gmail.com>
2026-02-09 20:36:27 -0300

    Sistema de loja
    Sistema de pagamento  de uma loja utilizando if, else e elif

---
+++ mod2/Exercício/ex044.py
@@ -0,0 +1,12 @@
+print ('{:=^40}'.format(' Loja do Chapéu ')) 
+
+compra = float (input ('Digite o valor da compra: R$ '))
+
+print (''' Formas de Pagamento:
+       [1] - à vista dinheiro ou cheque
+       [2] - à vista cartão
+       [3] - 2x no cartão 
+       [4] - 3x ou mais no cartão
+''')
+
+opcao = int(input(' Escolha uma opção: '))
