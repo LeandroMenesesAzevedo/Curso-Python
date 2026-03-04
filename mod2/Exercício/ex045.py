db91af3f9482809e17a53ea67b8d2e4e545c9d50
Leandro Meneses Azevedo <menezesdatoulon.lm@gmail.com>
2026-03-04 18:07:53 -0300

    Inicio do jogo Jokenpo

---
+++ mod2/Exercício/ex045.py
@@ -0,0 +1,11 @@
+from random import randint
+from time import sleep
+
+itens ('Pedra', 'Papel', 'Tesoura')
+
+print('''
+    [1] - Pedra
+    [2] - Papel
+    [3] - Tesoura
+''')
+op = int(input(' Escolha uma opção: '))
