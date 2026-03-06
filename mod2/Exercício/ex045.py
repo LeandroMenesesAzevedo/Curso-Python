166bf4ca0299fce445bb88c3477f4b6b793637d4
Leandro Meneses Azevedo <menezesdatoulon.lm@gmail.com>
2026-03-05 21:12:34 -0300

    realizando mais uma etapa do game jokenpo

--- mod2/Exercício/ex045.py
+++ mod2/Exercício/ex045.py
@@ -1,7 +1,8 @@
 from random import randint
 from time import sleep
 
-itens ('Pedra', 'Papel', 'Tesoura')
+itens =('Pedra', 'Papel', 'Tesoura')
+computador = randint(0, 2)
 
 print('''
     [1] - Pedra
@@ -9,3 +10,22 @@ print('''
     [3] - Tesoura
 ''')
 op = int(input(' Escolha uma opção: '))
+
+if op == 1:
+        if computador == 1:
+            print ('Você escolheu a opção {}'.format(op))
+            print (' O computador escolheu {}'.format(itens[computador]))
+            print ('DEU EMPATE')
+        elif computador == 2:
+            print ('Você escolheu a opção {}'.format(op))
+            print (' O computador escolheu {}'.format(itens[computador]))
+            print ('Você venceu')
+        elif computador == 3:
+            print('Você escolheu a opção {}'.format(op))
+            print ('O computador escolheu  {}'.format(itens[computador]))
+            print ( 'O computado venceu!!!')
+
+elif computador == 2:
+        print('teste')
+elif computador == 3:
+        print('teste')
