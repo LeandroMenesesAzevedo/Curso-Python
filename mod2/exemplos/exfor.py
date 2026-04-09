92833f520f757c0515cc2f3762d2a5efa236fc3f
Leandro Meneses Azevedo <110135554+LeandroMenesesAzevedo@users.noreply.github.com>
2026-04-09 20:15:55 -0300

    Retornando a rotina de estudos
    Exemplos utilizando o for, realizando o primeiro exercício contagem regressiva para o ano novo

--- mod2/exemplos/exfor.py
+++ mod2/exemplos/exfor.py
@@ -1,3 +1,3 @@
-for c in range(0, 6):
+for c in range(0, 10):
     print('Oi')
 print('Fim')
---
+++ mod2/exemplos/exfor2.py
@@ -0,0 +1,14 @@
+for c in range (6 , 0, -1):
+# Na contagem de 6 até 1 se não colocarmos o -1 o programa não executa o que está dentro do for 
+    print(c)
+print('FIM')
+
+for c in range (0, 7):
+# Contando de 0 a 6
+    print(c)
+print('FIM')
+
+for c in range (0 , 7 , 2):
+# O programa conta de 0 a 6 de dois  em dois
+    print(c)
+print('FIM')
