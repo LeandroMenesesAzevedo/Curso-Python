27f881c6250975e70e92a588b7224fd426b47400
Leandro Meneses Azevedo <menezesdatoulon.lm@gmail.com>
2026-06-23 21:12:30 -0300

    Exercício do relatório

---
+++ mod2/Exercício/ex55.py
@@ -0,0 +1,24 @@
+somaid = 0
+media = 0
+maioridadehomem = 0
+nomevelho = ''
+
+for c in range (1 , 5):
+    print ('------ {}º Pessoa -----'.format(c))
+    nome = str (input('Nome: ')).strip() # coloquei o strip para tirar os espaços
+    idade = int (input('Idade: '))
+    sex = str (input ('Sexo [ M/F ]: ')).strip() # coloquei o strip para tirar os espaços
+    somaid += idade
+
+    if c == 1 and sex in 'Mm':
+        maioridadehomem = idade
+        nomevelho = nome
+    
+    if sex in 'Mm' and idade > maioridadehomem:
+        maioridadehomem = idade
+        nomevelho = nome
+
+
+media = somaid/c
+print ('A media de idade é : {}'.format(media))
+print ('O homem mais velho é {} com {}'.format(nomevelho, maioridadehomem))
