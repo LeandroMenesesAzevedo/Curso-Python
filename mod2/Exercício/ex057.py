244405100c3177d875554ea6ddd15e5acd32350e
Leandro Meneses Azevedo <menezesdatoulon.lm@gmail.com>
2026-07-22 20:40:59 -0300

    Finalizado  programa do impar ou par

--- mod2/Exercício/ex057.py
+++ mod2/Exercício/ex057.py
@@ -1,10 +1,12 @@
-c = 1
+n = 1
 par = impar = 0
 
-while c <= 5:
-    if c != 0:
-        if c % 2 == 0:
+while n != 0:
+    n = int (input('Digite um valor: '))
+    if n != 0:
+        if n % 2 == 0:
             par +=1
         else:
             impar +=1
-print ('Foram digitados {} par e {}impar'.format(par, impar))
+
+print ('Foram digitados {} par e {} impar'.format(par, impar))
