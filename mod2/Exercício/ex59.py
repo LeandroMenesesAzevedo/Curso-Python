0d2d210ce5b85c7eabca91ab9e6098f512947229
Leandro Meneses Azevedo <110135554+LeandroMenesesAzevedo@users.noreply.github.com>
2026-08-05 20:31:29 -0300

    Finalizado o exercício do menu

--- mod2/Exercício/ex59.py
+++ mod2/Exercício/ex59.py
@@ -1,9 +1,65 @@
+import time
 n1 = int (input('Digite um valor: '))
-n2 = int (input('Digite outro valor'))
+n2 = int (input('Digite outro valor:  '))
+op = '0'
 
-print('=' * 50)
-print(' [1] Somar ')
-print(' [2] Subtrair ')
-print(' [3] Multiplicar ')
-print(' [4] Divisão ')
-print ('=' * 50)
+while op != 5:
+    print('=' * 50)
+    print('''        [1] Somar 
+        [2] Subtrair
+        [3] Multiplicar
+        [4] Divisão
+        [5] Finalizar o programa
+        ''')
+    print ('=' * 50)
+    op = int(input ('Escolha uma opção: '))
+
+    if op == 1:
+        soma = n1 +n2
+        print('...')
+        time.sleep(1)
+        print ('{} + {} = {}'.format(n1, n2 , soma))
+        print ('_' * 50)
+        time.sleep(2)
+        op = int(input ('Escolha uma opção: ')) 
+
+    elif op == 2:
+        sub = n1 - n2
+        print('...')
+        time.sleep(1)
+        print ('{} - {} = {}'.format(n1, n2, sub))
+        print('_ '*50)
+        time.sleep(2)
+        op = int(input('Escolha uma opção: '))
+
+    elif op == 3:
+        mult = n1 * n2
+        print('...')
+        time.sleep(1)
+        print('{} * {} = {}'.format(n1, n2, mult))
+        print ('_' *50)
+        time.sleep(2)
+        op = int(input('Escolha uma opção: '))
+
+    elif op == 4:
+        divisao = n1/n2
+        print('...')
+        time.sleep(1)
+        print('{} / {} = {}'.format(n1, n2, divisao))
+        print('_' * 50)
+        time.sleep(2)
+        op=(int(input('Escolha uma opção: ')))
+    
+    else :
+        print('...')
+        time.sleep(1)
+        print('Opção inválida')
+        print('_' * 50)
+        time.sleep(0.5)
+
+
+
+print('...')
+time.sleep (1)
+print('PROGRAMA FINALIZADO')
+
