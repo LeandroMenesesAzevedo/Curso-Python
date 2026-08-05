af39f9e989fd93de05d1ba425dd1df0f465df4be
Leandro Meneses Azevedo <110135554+LeandroMenesesAzevedo@users.noreply.github.com>
2026-08-05 19:35:41 -0300

    iniciando o menu

---
+++ mod2/Exercício/ex59.py
@@ -0,0 +1,9 @@
+n1 = int (input('Digite um valor: '))
+n2 = int (input('Digite outro valor'))
+
+print('=' * 50)
+print(' [1] Somar ')
+print(' [2] Subtrair ')
+print(' [3] Multiplicar ')
+print(' [4] Divisão ')
+print ('=' * 50)
