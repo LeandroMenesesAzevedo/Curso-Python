16594cc4fe1f41922ff46669b3fb03a196a78188
Leandro Meneses Azevedo <menezesdatoulon.lm@gmail.com>
2026-09-14 21:07:26 -0300

    Sequência de Fibonacci

---
+++ mod2/Exercício/ex63.py
@@ -0,0 +1,14 @@
+print ('_' *20)
+print('Sequência de fibonacci')
+print('_'*20)
+n = int(input('Quantos termos você deseja: '))
+t1 = 0
+t2 = 1
+print ('{} -> {}'.format(t1, t2), end="")
+t3 = 2
+while t3 < n:
+    t3 = t1 + t2
+    print(' -> {}'.format(t3), end="")
+    t1 = t2
+    t2 = t3
+print(' -> FIM')
