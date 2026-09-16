56dbe1d1dd346a5d2c24d9b9403f4b1a17d4c96f
Leandro Meneses Azevedo <menezesdatoulon.lm@gmail.com>
2026-09-15 21:20:46 -0300

    Finalizado o exercício 64

--- mod2/Exercício/ex64.py
+++ mod2/Exercício/ex64.py
@@ -1,8 +1,9 @@
 n = 0
 soma = 0
 cont = 0
+n = int (input(' Digite um número [Digite o número 999 para finalizar]: '))
 while n != 999:
-    n = int (input(' Digite um número [Digite o número 999 para finalizar]: '))
     soma += n
     cont += 1
+    n = int (input(' Digite um número [Digite o número 999 para finalizar]: '))
 print (' A quantidade de número digitado foi {} e a soma entre eles são: {}'.format(cont, soma))
